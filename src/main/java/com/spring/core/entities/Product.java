18c7266e570ea69e809e1013b81d2e27a6c68b29
unknown <trufanovwork@gmail.com>
2025-04-05 17:48:17 +0300

    init, add product with service

---
+++ src/main/java/com/spring/core/entities/Product.java
@@ -0,0 +1,4 @@
+package com.spring.core.entities;
+
+public class Product {
+}
---
+++ src/main/java/com/spring/core/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.spring.core.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductService {
+
+}
