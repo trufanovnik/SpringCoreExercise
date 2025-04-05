b308688e826982edd83a1dc81d8770d64b373ea6
unknown <trufanovwork@gmail.com>
2025-04-05 17:51:10 +0300

    add product fields

--- src/main/java/com/spring/core/entities/Product.java
+++ src/main/java/com/spring/core/entities/Product.java
@@ -1,4 +1,47 @@
 package com.spring.core.entities;
 
 public class Product {
+
+    private int id;
+    private String title;
+    private double cost;
+
+    public Product(int id, String title, double cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
 }
