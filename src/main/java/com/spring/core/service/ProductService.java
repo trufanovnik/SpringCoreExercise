818452ae98b90dcba21da99458bd604b0fde39f6
unknown <trufanovwork@gmail.com>
2025-04-06 22:40:15 +0300

    add product service

--- src/main/java/com/spring/core/service/ProductService.java
+++ src/main/java/com/spring/core/service/ProductService.java
@@ -1,8 +1,32 @@
 package com.spring.core.service;
 
-import org.springframework.stereotype.Component;
+import com.spring.core.entities.Product;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.List;
+
+@Service
 public class ProductService {
 
+    private List<Product> productList;
+
+    public void printAll(){
+        System.out.println("All products:");
+        productList.forEach(System.out::println);
+    }
+
+    public Product findByTitle(String title){
+        return productList.stream()
+                .filter(p -> p.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostConstruct
+    public void fillProducts(){
+        for (int i = 1; i < 11; i++) {
+            productList.add(new Product(i, "Product "+ i, 10+i));
+        }
+    }
 }
